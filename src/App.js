7182e078ba6d96098e1b4714742bc64f8d762c0e
Kaya Jackson <amanij7@gmail.com>
2018-06-22 13:52:40 -0400

    events revision

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
        <div className="App">
          <ul>
            { this.state.todos.map( (todo, index) =>
-             <ToDo key={index} description={todo.description} isCompleted={todo.isCompleted} toggleComplete={ () => this.toggleComplete(index) } />
+             <ToDo key={index} scoobyDoo={todo.description} isCompleted={todo.isCompleted} toggleComplete={ () => this.toggleComplete(index) } />
            )}
          </ul>
          <form onSubmit={ (e) => this.handleSubmit(e) }>
